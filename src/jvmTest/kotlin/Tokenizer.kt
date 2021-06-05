6e9e0f8b3887cc4475b8e5bde3c7c38cc107740e
Simone Giacomelli <simone.giacomelli@gmail.com>
2021-06-05 18:35:13 +0200

    tokenizer

--- src/jvmTest/kotlin/Tokenizer.kt
+++ src/jvmTest/kotlin/Tokenizer.kt
@@ -22,25 +22,37 @@ class TokenizerTest {
         assertEquals(listOf(0..1, 3..4, 6..7), target.map { it.range })
     }
 
+    @Test
+    fun punctuation() {
+        val target = target("aa:bb!cc")
+        assertEquals(listOf("aa", "bb", "cc"), target.map { it.text })
+        assertEquals(listOf(0..1, 3..4, 6..7), target.map { it.range })
+    }
+
+    @Test
+    fun multiplePunctuation() {
+        val target = target("aa:;bb!?cc")
+        assertEquals(listOf("aa", "bb", "cc"), target.map { it.text })
+        assertEquals(listOf(0..1, 4..5, 8..9), target.map { it.range })
+    }
+
     fun target(s: String) = tokenize(s).toList()
 
 }
 
 fun tokenize(s: String): Sequence<Token> {
-    val matches = """\p{Space}""".toRegex().findAll(s).iterator()
+    val matches = """\p{Space}|\p{Punct}""".toRegex().findAll(s).iterator()
     val sequence = sequence {
 
-//        suspend fun SequenceScope<IntRange>.y(r: IntRange) = yield(r)
-
-        if (!matches.hasNext()) return@sequence
+        suspend fun SequenceScope<IntRange>.y(r: IntRange) = if (r.isEmpty()) Unit else yield(r)
 
         var last = -1
         while (matches.hasNext()) {
             val m = matches.next()
-            yield(last + 1 until m.range.first)
+            y(last + 1 until m.range.first)
             last = m.range.last
         }
-        yield(last + 1 until s.length)
+        y(last + 1 until s.length)
     }
 
     return sequence.map { Token(s.substring(it), it) }
