21068ad77284033d75ee161b45d1a0519e943491
Simone Giacomelli <simone.giacomelli@gmail.com>
2021-06-05 21:41:37 +0200

    kotlin on trix

--- src/jsMain/kotlin/client.kt
+++ src/jsMain/kotlin/client.kt
@@ -3,10 +3,19 @@ import kotlinx.browser.window
 import kotlinx.html.div
 import kotlinx.html.dom.append
 import org.w3c.dom.Node
+import org.w3c.dom.events.Event
 
 fun main() {
-    println("Js v0.4")
+    println("Js v0.5")
     window.onload = { document.body?.sayHello() }
+    val trix = document.getElementById("trix") ?: error("element trix not found")
+    trix.addEventListener("trix-change", { event: Event ->
+        console.log("kt trix-change")
+        val trixdyn: dynamic = trix
+        val editor = trixdyn.editor
+        val content = editor.getDocument().toString()
+        console.log(content)
+    })
 }
 
 fun Node.sayHello() {
