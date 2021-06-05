9d218cebd5f6711a7847688de2c3e9600503770c
Simone Giacomelli <simone.giacomelli@gmail.com>
2021-06-05 20:41:39 +0200

    html integration

--- src/jvmMain/kotlin/JvmMain.kt
+++ src/jvmMain/kotlin/JvmMain.kt
@@ -5,26 +5,9 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
-import kotlinx.html.*
 
 const val static = "static"
-fun HTML.index() {
-    val jsFolder = "$static/js"
-    head {
-        title("Hello from Ktor!")
-        script(src = "$jsFolder/trix.js") {}
-        link(href = "$jsFolder/trix.css", rel = "stylesheet", type = "text/css")
-    }
-    body {
-        div {
-            +"Hello from Ktor"
-        }
-        div {
-            id = "root"
-        }
-        script(src = "$jsFolder/js.js") {}
-    }
-}
+
 
 fun main() {
     embeddedServer(CIO, port = 8080, module = Application::module)
@@ -35,13 +18,12 @@ fun main() {
 fun Application.module() {
     routing {
         get("/") {
-            //call.respondHtml(HttpStatusCode.OK, HTML::index)
-            val content = Application::class.java.getResource("/js/index.html").readText()
-            call.respondText(
-                content.replace(
-                    "<!-- base -->", """<base href="static/js/">"""
-                ), contentType = ContentType.Text.Html
-            )
+            val marker = "<!-- base -->"
+            val resource = Application::class.java.getResource("/js/index.html").readText()
+            check(resource.contains(marker))
+            val content = resource
+                .replace(marker, """<base href="static/js/">""")
+            call.respondText(content, contentType = ContentType.Text.Html)
         }
         static("/$static") {
             resources()
